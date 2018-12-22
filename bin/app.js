a12c6e6f9ac49975680989c1ec359f6791a228a3
Elsiddig Ahmed <elsiddig190@gmail.com>
2018-12-22 13:08:15 +0200

    .

--- bin/app.js
+++ bin/app.js
@@ -1,33 +1,33 @@
-let express = require('express')
-let bodyParser = require('body-parser')
-let path = require('path')
-let router = require('./router/route')
-let nubiaRouter = require('./router/nubia-route')
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const router = require('./router/route')
+const nubiaRouter = require('./router/nubia-route')
 
 // invoke express minimalistic liberary
-let app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+const app = express()
 
 // use external router module
 app.use(router)
 app.use('/nubia', nubiaRouter)
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
 //setup a static resources
-app.use(express.static(path.join('home')))
+app.use(express.static(path.join(__dirname, 'home')))
 
 //setup a view engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'home'))
 
-var port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 //listen on the port in order to run the app in the browser
-app.listen(port, function(){
-	console.log(`server starts at port ${port}`)
+app.listen(port, () => {
+	console.log(`You can type in the browser http://127.0.0.1:${port} to run the application!`)
 })
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
 	res.render('index', {
 		title: 'Script House Technical Team & IT Support'
 	})
